d7b6ccf0b651788bba0eed2aa2e1f57a13bc3ab1
Emil <jeziorski.emil@gmail.com>
2020-03-17 12:30:44 +0100

    Updates

---
+++ precache-manifest.3559043379bf0cdad937eca62f941243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1bf546c470cd4a6de372854d4c7875",
+    "url": "/company-incomes/index.html"
+  },
+  {
+    "revision": "01ca6c21551f4fb72712",
+    "url": "/company-incomes/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a00ff533c19a50259529",
+    "url": "/company-incomes/static/css/main.449e21ac.chunk.css"
+  },
+  {
+    "revision": "01ca6c21551f4fb72712",
+    "url": "/company-incomes/static/js/2.da8e2f94.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/company-incomes/static/js/2.da8e2f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00ff533c19a50259529",
+    "url": "/company-incomes/static/js/main.1afca88d.chunk.js"
+  },
+  {
+    "revision": "8077f9ecc62e36460145",
+    "url": "/company-incomes/static/js/runtime-main.0b119513.js"
+  }
+]);
